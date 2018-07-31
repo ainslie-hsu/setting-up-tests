dbc7ca4e98b49ae6c3c4bbbde657c1652d43b4db
JohnWebb4 <John.Webb94@gmail.com>
2018-07-30 17:08:37 -0700

    Setup enzyme

---
+++ setupTests.js
@@ -0,0 +1,5 @@
+/* eslint import/no-extraneous-dependencies: 0 */
+const Enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+
+Enzyme.configure({ adapter: new Adapter() });
